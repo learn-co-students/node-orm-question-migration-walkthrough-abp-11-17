8551f0ed72d04d706668cd1e0811a3f7254580f7
Maverick Tesini <mavericktesini@gmail.com>
2017-11-17 00:28:26 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -1,9 +1,29 @@
 'use strict';
-
+//pre coded
 const db = require("../config/db")
+//added next
+const sqlite3 = require('sqlite3').verbose()
 
+//added class
 class Question{
 
+  static CreateTable() {
+    const sql = `
+      CREATE TABLE IF NOT EXISTS questions (
+        id INTEGER PRIMARY KEY,
+        content TEXT
+      )
+    `
+    console.log("Preparing to create Table questions...");
+
+    return new Promise(function(resolve){
+      db.run(sql, function(){
+        console.log("..questions table created!");
+        resolve("success");
+      })
+    })
+
+  }
 }
 
 
