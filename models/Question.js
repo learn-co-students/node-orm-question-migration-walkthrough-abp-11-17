3919fc6089b19623d560c093e86d37f939b37752
Jacob Denbeaux <jdenbeaux19@wooster.edu>
2017-11-18 20:08:46 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -3,7 +3,14 @@
 const db = require("../config/db")
 
 class Question{
-
+  static CreateTable() {
+    return new Promise(function(resolve) {
+      const query = 'CREATE TABLE questions (id INTEGER PRIMARY KEY, content TEXT)'
+      db.run(query, function() {
+        resolve('made the table')
+      })
+    })
+  }
 }
 
 
