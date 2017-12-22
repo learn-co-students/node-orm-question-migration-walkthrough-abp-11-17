cb7555833c8bdceb9556d734ca880a5d19ded7d0
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-22 23:34:52 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -3,7 +3,19 @@
 const db = require("../config/db")
 
 class Question{
+  static CreateTable(){
+    return new Promise(function(resolve){
+      const sql = `CREATE TABLE questions(
+        id INTEGER PRIMARY KEY,
+        content TEXT
+      )
 
+        `
+      db.run(sql, function(){
+        resolve("Questions table created")
+      })
+    })
+  }
 }
 
 
